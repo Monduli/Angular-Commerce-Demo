744f0e855b990e3681d416917359640acf3a2124
Monduli <dcglendon@gmail.com>
2023-08-22 17:03:40 -0400

    Finished course, further work tba

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle: string = 'Product Detail'
+  pageTitle: string = 'Product Detail';
+  product: IProduct | undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, 
+              private router: Router) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `: ${id}`;
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
